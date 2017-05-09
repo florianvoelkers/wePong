16effb0f35f0fca4c3840a2fe0df3a678e59f278
Nikolai Stecker <Nikolai.Stecker@gmx.de>
2017-05-09 16:41:48 +0200

    Schläger und der Ball werden jetzt mit Funktionen gezeichnet um später leichte Bewegung zu ermöglichen

--- Game/wePong.py
+++ Game/wePong.py
@@ -10,33 +10,89 @@ FPS = 60
 WINDOWWIDTH = 1920
 WINDOWHEIGHT = 1080
 
+# Schlägergröße
+PADDLESIZE = WINDOWHEIGHT / 2
+PADDLEOFFSET = 20
+LINETHICKNESS = 4
+
+# Farben vor definieren
+WHITE = [255, 255, 255]
+RED = [255, 0, 0]
+BLUE = [0,0,255]
+GREEN = [0,255,0]
+
+
+# Arena zeichnen
+def drawArena():
+    SCREEN.fill((0,0,0))
+    net = pygame.draw.rect(SCREEN,GREEN,(WINDOWWIDTH/2-4,0,8,WINDOWHEIGHT))
+
+    # Rand oben und unten
+    uppeREDge = pygame.draw.rect(SCREEN,GREEN,(0,WINDOWHEIGHT-LINETHICKNESS,WINDOWWIDTH,LINETHICKNESS))
+    loweREDge = pygame.draw.rect(SCREEN,GREEN,(0,0,WINDOWWIDTH,LINETHICKNESS))
+
+# Schläger zeichnen
+def drawPaddle(paddle):
+    #Hält den Schläger auf aus dem Screen zu verschwinden
+    if paddle.bottom > WINDOWHEIGHT - LINETHICKNESS:
+        paddle.bottom = WINDOWHEIGHT - LINETHICKNESS
+    elif paddle.top < LINETHICKNESS:
+        paddle.top = LINETHICKNESS
+
+    pygame.draw.rect(SCREEN, WHITE, paddle)
+  
+# Ball zeichnen
+def drawBall(ballX,ballY):
+    pygame.draw.circle(SCREEN, WHITE, (int(ballX),int(ballY)), LINETHICKNESS*4)
+
+
+
+
 def main():
-    pygame.init()
+    pygame.init()  
 
-    # Farben vor definieren
-    white = [255, 255, 255]
-    red = [255, 0, 0]
-    blue = [0,0,255]
-    green = [0,255,0]
+    global SCREEN
 
-    FPSCLOCK = pygame.time.Clock()
-    screen = pygame.display.set_mode((WINDOWWIDTH,WINDOWHEIGHT), pygame.FULLSCREEN) 
+    SCREEN = pygame.display.set_mode((WINDOWWIDTH,WINDOWHEIGHT), pygame.FULLSCREEN)    
     pygame.display.set_caption('wePong')
 
-    # Spielfläche in einer Farbe
-    screen.fill(blue)
-    # Netz in der Mitte
-    net = pygame.draw.rect(screen,green,(WINDOWWIDTH/2-4,0,8,WINDOWHEIGHT))
-    # Rand oben und unten
-    upperEdge = pygame.draw.rect(screen,green,(0,WINDOWHEIGHT-4,WINDOWWIDTH,4))
-    lowerEdge = pygame.draw.rect(screen,green,(0,0,WINDOWWIDTH,4))
+    FPSCLOCK = pygame.time.Clock()
 
+    # Spielfläche in einer Farbe
+    SCREEN.fill(BLUE)
+    # Netz in der Mitte
+    net = pygame.draw.rect(SCREEN,GREEN,(WINDOWWIDTH/2-4,0,8,WINDOWHEIGHT))
+    # Rand oben und unten
+    uppeREDge = pygame.draw.rect(SCREEN,GREEN,(0,WINDOWHEIGHT-LINETHICKNESS,WINDOWWIDTH,LINETHICKNESS))
+    loweREDge = pygame.draw.rect(SCREEN,GREEN,(0,0,WINDOWWIDTH,LINETHICKNESS))
+
+    # Ball Startposition
+    ballX = WINDOWWIDTH/2 - LINETHICKNESS/2
+    ballY = WINDOWHEIGHT/2 - LINETHICKNESS/2
+
+    # Startposition der Schläger
+    playerOnePosition = (WINDOWHEIGHT - PADDLESIZE) /2
+    playerTwoPosition = (WINDOWHEIGHT - PADDLESIZE) /2
+
+    # Erstellen der Schläger
+    paddle1 = pygame.Rect(PADDLEOFFSET,playerOnePosition, LINETHICKNESS*3,PADDLESIZE)
+    paddle2 = pygame.Rect(WINDOWWIDTH - PADDLEOFFSET , playerTwoPosition, LINETHICKNESS*3,PADDLESIZE)
+    
+    drawArena()
+    drawPaddle(paddle1)
+    drawPaddle(paddle2)
+    drawBall(ballX,ballY)
 
     while True: 
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
+
+        drawArena()
+        drawPaddle(paddle1)
+        drawPaddle(paddle2)
+        drawBall(ballX,ballY)
 
         pygame.display.update()
         FPSCLOCK.tick(FPS)

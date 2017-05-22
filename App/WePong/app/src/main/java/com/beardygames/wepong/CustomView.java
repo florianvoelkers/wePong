91d5ad509cda6a5000c468bcbb0db768353cca41
Florian Voelkers <florian.voelkers@mail.de>
2017-05-22 15:32:31 +0200

    Hintergrund für den Startbildschirm steht, jetzt werden die Buttons hinzugefügt

--- App/WePong/app/src/main/java/com/beardygames/wepong/CustomView.java
+++ App/WePong/app/src/main/java/com/beardygames/wepong/CustomView.java
@@ -1,14 +1,21 @@
 package com.beardygames.wepong;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Picture;
 import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
 
 public class CustomView extends View {
+
+    private Rect joinGameButton;
 
     public CustomView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -23,8 +30,33 @@ public class CustomView extends View {
         paint.setColor(Color.WHITE);
         paint.setStrokeWidth(9);
 
-        canvas.drawLine(5, 0, 5, canvas.getHeight(), paint);
-        canvas.drawLine(canvas.getWidth() - 5, 0, canvas.getWidth() - 5, canvas.getHeight(), paint);
+        int centerX = (int) (canvas.getWidth() * 0.5f);
+        int centerY = (int) (canvas.getHeight() * 0.5f);
+        int height = canvas.getHeight();
+        int width = canvas.getWidth();
 
+        canvas.drawLine(5, 0, 5, height, paint);
+        canvas.drawLine(width - 5, 0, width - 5, height, paint);
+        canvas.drawLine(0, centerY - 5,width, centerY - 5, paint);
+
+        Bitmap title = BitmapFactory.decodeResource(getResources(), R.drawable.title);
+        canvas.drawBitmap(title, centerX - title.getWidth() * 0.5f, height * 0.25f - title.getHeight() * 0.5f, paint);
+
+        paint.setStyle(Paint.Style.STROKE);
+        joinGameButton = new Rect(160, centerY + 120, width - 160, centerY + 280);
+        canvas.drawRect(joinGameButton, paint);
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+
+        if (joinGameButton.contains(x, y)){
+            
+        }
+
+        return super.onTouchEvent(event);
     }
 }
--- App/WePong/app/src/main/java/com/beardygames/wepong/MainActivity.java
+++ App/WePong/app/src/main/java/com/beardygames/wepong/MainActivity.java
@@ -3,6 +3,7 @@ package com.beardygames.wepong;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         hideBars();
-
 
         view = (CustomView)findViewById(R.id.customView);
     }

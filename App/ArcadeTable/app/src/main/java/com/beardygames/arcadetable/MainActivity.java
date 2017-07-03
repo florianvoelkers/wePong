76ff77ac4384c390935330c02f9cbb9009b80adb
Florian Voelkers <florian.voelkers@mail.de>
2017-07-03 13:32:24 +0200

    Start der Entwicklung der neuen App - ArcadeTable - Fullscreen und System Bar Handling implementiert

---
+++ App/ArcadeTable/app/src/main/java/com/beardygames/arcadetable/MainActivity.java
@@ -0,0 +1,43 @@
+package com.beardygames.arcadetable;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private View decorView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        decorView = getWindow().getDecorView();
+        // Hide both the navigation bar and the status bar.
+        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+        // a general rule, you should design your app to hide the status bar whenever you
+        // hide the navigation bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+    }
+
+    // The IMMERSIVE_STICKY flag, and the user swipes to display the system bars.
+    // Semi-transparent bars temporarily appear and then hide again.
+    // The act of swiping doesn't clear any flags, nor does it trigger your system UI visibility change listeners,
+    // because the transient appearance of the system bars isn't considered a UI visibility change.
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+}
--- App/WePong/app/src/main/java/com/beardygames/wepong/MainView.java
+++ App/WePong/app/src/main/java/com/beardygames/wepong/MainView.java
@@ -12,6 +12,9 @@ import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 
 public class MainView extends View {
@@ -39,37 +42,8 @@ public class MainView extends View {
         int height = canvas.getHeight();
         int width = canvas.getWidth();
 
-        canvas.drawLine(5, 0, 5, height, paint);
-        canvas.drawLine(width - 5, 0, width - 5, height, paint);
-        canvas.drawLine(0, centerY - 5,width, centerY - 5, paint);
-
-        paint.setTextSize(172);
-        paint.setTextAlign(Paint.Align.CENTER);
-        canvas.drawText("wePong", centerX, centerY * 0.42f, paint);
-
-        Bitmap title = BitmapFactory.decodeResource(getResources(), R.drawable.title);
-        canvas.drawBitmap(title, centerX - title.getWidth() * 0.5f, height * 0.25f - title.getHeight() * 0.5f, paint);
-
-        paint.setStyle(Paint.Style.STROKE);
-
-        joinGameButton = new Rect(160, centerY + 80, width - 160, centerY + 220);
-        canvas.drawRect(joinGameButton, paint);
-
-        instructionsButton = new Rect(160, centerY + 260, width - 160, centerY + 400);
-        canvas.drawRect(instructionsButton, paint);
-
-        setNameButton = new Rect(160, centerY + 440, width - 160, centerY + 580);
-        canvas.drawRect(setNameButton, paint);
-
-        exitButton = new Rect(160, centerY + 620, width - 160, centerY + 760);
-        canvas.drawRect(exitButton, paint);
-
-        paint.setStyle(Paint.Style.FILL);
-        paint.setTextSize(72);
-        canvas.drawText("Join Game", centerX, joinGameButton.centerY() + 16, paint);
-        canvas.drawText("Instructions", centerX, instructionsButton.centerY() + 16, paint);
-        canvas.drawText("Set Name", centerX, setNameButton.centerY() + 16, paint);
-        canvas.drawText("Exit", centerX, exitButton.centerY() + 16, paint);
+        Bitmap background = BitmapFactory.decodeResource(getResources(), R.drawable.background);
+        canvas.drawBitmap(background, centerX - background.getWidth() * 0.5f, centerY - background.getHeight() * 0.5f, paint);
     }
 
     @Override

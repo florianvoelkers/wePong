a1570ae29f2458096d702f2dfe919b0ef7ff80bc
Flo-Laptop <florian.voelkers@mail.de>
2017-07-04 19:10:50 +0200

    Code für die Sensoren hinzugefügt zur Pong Activity

--- App/ArcadeTable/app/src/main/java/com/beardygames/arcadetable/PongActivity.java
+++ App/ArcadeTable/app/src/main/java/com/beardygames/arcadetable/PongActivity.java
@@ -1,18 +1,34 @@
 package com.beardygames.arcadetable;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
 
-public class PongActivity extends AppCompatActivity {
+public class PongActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private Sensor rotationSensor;
+    private double pitch;
 
     private View decorView;
+    private ImageView playerOne;
+    private ImageView playerTwo;
+    private boolean touchReady;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pong);
+
+        touchReady = true;
 
         decorView = getWindow().getDecorView();
         // Hide both the navigation bar and the status bar.
@@ -21,7 +37,33 @@ public class PongActivity extends AppCompatActivity {
         // hide the navigation bar.
         int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 | View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        playerOne = (ImageView) findViewById(R.id.player_one);
+        playerTwo = (ImageView) findViewById(R.id.player_two);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        rotationSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GAME_ROTATION_VECTOR);
+        pitch = 0;
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_GAME_ROTATION_VECTOR) {
+            // Umrechnung Rotationsvektor --> Rotationsmatrix
+            float[] rotationMatrix = new float[9];
+            SensorManager.getRotationMatrixFromVector(rotationMatrix, event.values);
+            //Umrechnung Rotationsmatrix --> Orientierung (3 Winkel)
+            final float[] orientationAngles = new float[3];
+            SensorManager.getOrientation(rotationMatrix, orientationAngles);
+
+            pitch = orientationAngles[1] * 360 / (2 * Math.PI);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
     }
 
     // The IMMERSIVE_STICKY flag, and the user swipes to display the system bars.
@@ -40,5 +82,35 @@ public class PongActivity extends AppCompatActivity {
                             | View.SYSTEM_UI_FLAG_FULLSCREEN
                             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (touchReady) {
+            int x = (int) event.getX();
+            int width = MainActivity.getScreenWidth();
+
+            if (x >= width * 0.5) {
+                playerOne.setVisibility(View.INVISIBLE);
+                touchReady = false;
+            } else {
+                playerTwo.setVisibility(View.INVISIBLE);
+                touchReady = false;
+            }
+        }
+
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, rotationSensor, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
     }
 }
